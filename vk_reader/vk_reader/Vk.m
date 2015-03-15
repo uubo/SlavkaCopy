b1ae35162df001ffe2876954083693b2fc23f444
Vlad Agiyevich <sky-mart@hotmail.com>
2015-03-15 14:31:47 +0300

    Свежак

--- vk_reader/vk_reader/Vk.m
+++ vk_reader/vk_reader/Vk.m
@@ -10,7 +10,7 @@
 
 @interface Vk() <NSURLConnectionDelegate>
 
-@property (nonatomic, strong) NSData* responseData;
+@property (nonatomic, strong) NSString* responseString;
 
 @end
 
@@ -21,19 +21,33 @@
     self = [super init];
     if (self){
         
-        NSString *accesToken = @"b574f1a4d8606febb7ff3fa8653b58c8ee2ab63dd275ca2cf8cb7eefb6b1d712cd03ad31c34f83929ef27&access_token=72dae3f40baee22a38584e07c431";
-        NSString *urlString = [@"api.vk.com/method/messages.get.xml?out=0&count=10&access_token=" stringByAppendingString:accesToken];
-        NSURL *url = [NSURL URLWithString:urlString];
-        
-        NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:60.0];
-        
-        [request setHTTPMethod:@"GET"];
-        [[NSURLConnection alloc] initWithRequest:request delegate:self];
-        
-        
+        NSString *accesToken = @"b574f1a4d8606febb7ff3fa8653b58c8ee2ab63dd275ca2cf8cb7eefb6b1d712cd03ad31c34f83929ef27";
+        NSString *urlString = [@"https://api.vk.com/method/messages.get.xml?out=0&count=10&access_token=" stringByAppendingString:accesToken];
+//        NSURL *url = [NSURL URLWithString:@"http://google.com"];
+//        
+//        NSURLRequest *request = [NSURLRequest requestWithURL:url];
+//        NSURLConnection *connection = [NSURLConnection connectionWithRequest:request delegate:self];
+//        [connection start];
+//        NSURL *url = [NSURL URLWithString:@"http://ya.ru"];
+//       NSData *data = [NSData dataWithContentsOfURL:url];
+//       NSString *ret = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+//       NSLog(@"ret=%@", ret);
     }
     
     return self;
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
+{
+    if ([response isMemberOfClass:[NSHTTPURLResponse class]]) {
+        NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
+        NSInteger statusCode = [httpResponse statusCode];
+    }
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
+{
+    self.responseString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
 }
 
 
--- vk_reader/vk_reader/main.m
+++ vk_reader/vk_reader/main.m
@@ -7,19 +7,11 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Vk.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        
-        NSString *accesToken = @"b574f1a4d8606febb7ff3fa8653b58c8ee2ab63dd275ca2cf8cb7eefb6b1d712cd03ad31c34f83929ef27&access_token=72dae3f40baee22a38584e07c431";
-        NSString *urlString = [@"api.vk.com/method/messages.get.xml?out=0&count=10&access_token=" stringByAppendingString:accesToken];
-        NSURL *url = [NSURL URLWithString:urlString];
-        
-        NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:60.0];
-        
-        [request setHTTPMethod:@"GET"];
-        [[NSURLConnection alloc] initWithRequest:request delegate:self];
-        
+        Vk *vk = [[Vk alloc] init];
         
     }
     return 0;
