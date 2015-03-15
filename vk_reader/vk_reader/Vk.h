276c594c1b092939ad01c853324d9b8bebf65395
Jack Klyshko <klyshko.jack@gmail.com>
2015-03-15 14:10:52 +0300

    Added request

---
+++ vk_reader/vk_reader/Vk.h
@@ -0,0 +1,18 @@
+//
+//  Vk.h
+//  vk_reader
+//
+//  Created by Евгений Клышко on 15.03.15.
+//  Copyright (c) 2015 uubo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Vk : NSObject
+
+
+-(id) init;
+
+@end
+
+
--- vk_reader/vk_reader/Vk.m
+++ vk_reader/vk_reader/Vk.m
@@ -1,15 +1,25 @@
 //
-//  main.m
+//  Vk.m
 //  vk_reader
 //
 //  Created by Евгений Клышко on 15.03.15.
 //  Copyright (c) 2015 uubo. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import "Vk.h"
 
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
+@interface Vk() <NSURLConnectionDelegate>
+
+@property (nonatomic, strong) NSData* responseData;
+
+@end
+
+@implementation Vk
+
+-(id)init
+{
+    self = [super init];
+    if (self){
         
         NSString *accesToken = @"b574f1a4d8606febb7ff3fa8653b58c8ee2ab63dd275ca2cf8cb7eefb6b1d712cd03ad31c34f83929ef27&access_token=72dae3f40baee22a38584e07c431";
         NSString *urlString = [@"api.vk.com/method/messages.get.xml?out=0&count=10&access_token=" stringByAppendingString:accesToken];
@@ -22,5 +32,12 @@ int main(int argc, const char * argv[]) {
         
         
     }
-    return 0;
+    
+    return self;
 }
+
+
+
+
+@end
+
